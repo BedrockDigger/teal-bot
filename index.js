cc5972d28d6ea637fde41f509f53a3056ecc0240
BedrockDigger <callmebedrockdigger@gmail.com>
2021-09-11 18:59:05 +0800

    我鞠躬尽瘁了

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const HTMLParser = require('node-html-parser');
-const request = require('request');
+const superagent = require('superagent');
 const md5 = require('md5');
 require('dotenv').config()
 
@@ -15,24 +15,12 @@ let queryStatusId = ''
 let queryStatusContent = '' // stripped by value assignment
 let queryUsername = ''
 let queryReplyStatusId = '' // optional
+let selfStatusId = '' // aka the ID of the last posted thing
 let queryTagsArray = [] // mandatory, but can be empty
 let queryCommandsArray = []
 
-// static request option objects (dynamic ones are living in the funcs that call them)
-const requestOptions = {
-  getNotifications: {
-    method: 'GET',
-    url: 'https://erica.moe/api/v1/notifications',
-    headers: {
-      'Authorization': token,
-      'Content-Type': 'multipart/form-data'
-    },
-    formData: { limit: '1' }
-  },
-}
-
-// commands
-const commands = ['thelp', 'techo', 'ttrans', 'tchat']
+// commands except tchat
+const commands = ['thelp', 'techo', 'ttrans']
 
 // translate target language abbrs
 const languages = [
@@ -45,119 +33,102 @@ const languages = [
 ]
 
 function main() {
-  request(requestOptions.getNotifications, function (error, _response, body) {
-    if (error) {
-      throw new Error(error);
-    }
-    const queryObject = JSON.parse(body)[0]
-    queryId = queryObject.id
-    if (queryId !== currentQueryId) {
-      queryUsername = queryObject.account.username
-      if (queryObject.type === 'mention') {
-        queryStatusId = queryObject.status.id
-        queryStatusContent = stripContent(queryObject.status.content, true)
-        queryTagsArray = queryObject.status.tags.map(tagObject => tagObject.name)
-        queryCommandsArray = queryTagsArray.filter(x => commands.includes(x));
-        if (queryCommandsArray.length !== 1) {
-          postStatus("我比较傻，只能听懂不多不少一个命令 :blobmiou:", true)
-        }
-        else if (hasCommand('techo')) {
-          commandEcho()
-        }
-        else if (hasCommand('thelp')) {
-          commandHelp()
-        }
-        else if (hasCommand('ttrans')) {
-          const querylanguageArray = queryTagsArray.filter(x => languages.includes(x));
-          if (querylanguageArray.length > 1) {
-            postStatus("我比较傻，一次只能翻译成一种语言 :blobmiou:", true)
+  superagent.get('https://erica.moe/api/v1/notifications')
+    .set('Authorization', token)
+    .set('Content-Type', 'multipart/form-data')
+    .field('limit', '1')
+    .then((res) => {
+      const queryObject = JSON.parse(res.text)[0]
+      queryId = queryObject.id
+      if (queryId !== currentQueryId) {
+        queryUsername = queryObject.account.username
+        if (queryObject.type === 'mention') {
+          queryStatusId = queryObject.status.id
+          queryStatusContent = stripContent(queryObject.status.content, true)
+          queryTagsArray = queryObject.status.tags.map(tagObject => tagObject.name)
+          queryCommandsArray = queryTagsArray.filter(x => commands.includes(x));
+          if (queryCommandsArray.length > 1) {
+            postStatus("我比较傻，只能听懂一个命令 :blobmiou:", true, false)
+          }
+          else if (hasCommand('techo')) {
+            commandEcho()
+          }
+          else if (hasCommand('thelp')) {
+            commandHelp()
+          }
+          else if (hasCommand('ttrans')) {
+            const querylanguageArray = queryTagsArray.filter(x => languages.includes(x));
+            if (querylanguageArray.length > 1) {
+              postStatus("我比较傻，一次只能翻译成一种语言 :blobmiou:", true, false)
+            }
+            else {
+              queryReplyStatusId = queryObject.status.in_reply_to_id
+              commandTranslate(querylanguageArray[0])
+            }
           }
           else {
-            queryReplyStatusId = queryObject.status.in_reply_to_id
-            commandTranslate(querylanguageArray[0])
+            commandChat()
           }
         }
-        else if (hasCommand('tchat')) {
-          commandChat()
-        }
-        else {
-          postStatus("这个命令 @estel_de_hikari 没教过我，怪他咯～ :blobmiou:", true)
+        else if (queryObject.type === 'follow') {
+          postStatus("Teal Bot 现在好开心，因为被你 follow 啦！ :blobcatrainbow: \n\
+  发布一则内容为 @teal #thelp 的嘟文来查看我的技能！", false, false)
         }
       }
-      else if (queryObject.type === 'follow') {
-        postStatus("Teal Bot 现在好开心，因为被你 follow 啦！ :blobcatrainbow: \n\
-发布一则内容为 @teal #thelp 的嘟文来查看我的技能！", false)
-      }
-    }
-    currentQueryId = queryId
-  }
-  );
-  setTimeout(main, 1000);
+      currentQueryId = queryId
+    })
+  setTimeout(main, 5000);
 }
 
 // command functions
 
 async function commandEcho() {
   const message = "刚才你对我说：\n" + queryStatusContent
-  postStatus(message, true)
+  postStatus(message, true, false)
 }
 
 async function commandChat() {
-  const options = {
-    method: 'GET',
-    url: 'http://api.qingyunke.com/api.php',
-    qs: { key: 'free', appid: '0', msg: queryStatusContent }
-  };
-
-  request(options, function (error, _response, body) {
-    if (error) throw new Error(error);
-    const answer = JSON.parse(body).content
-    postStatus(answer, true)
-  });
-
+  superagent.get('http://api.qingyunke.com/api.php')
+    .query({
+      key: 'free',
+      appid: '0',
+      msg: queryStatusContent
+    })
+    .then((res) => {
+      const answer = JSON.parse(res.text).content
+      postStatus(answer, true, false)
+    })
 }
 
 async function commandTranslate(lang) {
   let originalText = ""
-  const getStatusOptions = {
-    method: 'GET',
-    url: 'https://erica.moe/api/v1/statuses/' + queryReplyStatusId,
-    headers: { Authorization: token }
+  const sourceBody = JSON.parse((await superagent.get('https://erica.moe/api/v1/statuses/' + queryReplyStatusId)
+    .set('Authorization', token)).text)
+  console.log("-----getStatus-----\n" + JSON.stringify(sourceBody) + "\n");
+  originalText = stripContent(sourceBody.content, true)
+  const randNum = getRandomInt(1, 99999);
+  const sign = md5(appid + originalText + randNum + key);
+  const translatedBody = JSON.parse((await superagent.get('https://fanyi-api.baidu.com/api/trans/vip/translate')
+    .query({
+      q: originalText,
+      from: 'auto',
+      to: lang ? lang : 'zh',
+      appid: appid,
+      salt: randNum,
+      sign: sign
+    })).text)
+  console.log(translatedBody)
+  if (translatedBody.error_code) {
+    const errorReport = "百度翻译 API 报错啦。以下是错误信息：\n" + translatedBody.toString()
+    postStatus(errorReport, true, false)
   }
-  request(getStatusOptions, function (error, _response, body) {
-    if (error) throw new Error(error);
-    originalText = stripContent(JSON.parse(body).content, true)
-    const randNum = getRandomInt(1, 99999);
-    const sign = md5(appid + originalText + randNum + key);
-    const getTranslateOptions = {
-      method: 'GET',
-      url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
-      qs: {
-        q: originalText,
-        from: 'auto',
-        to: lang ? lang : 'zh',
-        appid: appid,
-        salt: randNum,
-        sign: sign
-      }
-    };
-    request(getTranslateOptions, function (error, _response, body) {
-      if (error) throw new Error(error);
-      console.log(body);
-      const translatedObject = JSON.parse(body)
-      if (translatedObject.error_code) {
-        const errorReport = "百度翻译 API 报错啦。以下是错误信息：\n" +
-          translatedObject.error_code + " " + translatedObject.error_msg
-        postStatus(errorReport, true)
-      }
-      else {
-        const result = "百度翻译说，上面那句话的意思是：\n" + translatedObject.trans_result[0].dst
-        postStatus(result, true)
-      }
+  else {
+    const targetString = translatedBody.trans_result[0].dst
+    await postStatus('百度翻译说，上面那段话的意思是：\n', true, false)
+    for (let i = 0; i < targetString.length; i += 480) {
+      await postStatus(targetString.substring(i, i + 480), false, true)
     }
-    )
   }
-  )
 }
 
 async function commandHelp() {
@@ -175,12 +146,12 @@ async function commandHelp() {
 \n\
 #thelp: 显示本则帮助\n\
 #techo: 回显你发给我的嘟文\n\
-#tchat: 和我一起快乐聊天（目前使用青云客的菲菲人工智障）\n\
-#ttrans: 翻译指定嘟文（目前使用百度翻译）\n\
+#tchat: 这个技能使用时不用加技能标签，使用时和我一起快乐聊天（目前使用青云客的菲菲人工智障）\n\
+#ttrans: 翻译指定嘟文（目前使用百度翻译），嘟文的主人如果锁嘟需要接受 Teal Bot 的关注才能翻译\n\
 \n\
 关于每个技能的详细用法，请参考 https://github.com/BedrockDigger/teal-bot/blob/master/README.md'
 
-  postStatus(message, true)
+  postStatus(message, true, false)
 }
 
 // utility functions
@@ -197,24 +168,18 @@ function hasCommand(commandName) {
   return queryCommandsArray.indexOf(commandName) > -1;
 }
 
-async function postStatus(message, doReply) {
+async function postStatus(message, doReply, doReplySelf) {
   const content = "@" + queryUsername + " " + message
-  const postStatusOptions = {
-    method: 'POST',
-    url: 'https://erica.moe/api/v1/statuses',
-    headers: {
-      'Authorization': token,
-      'Content-Type': 'multipart/form-data'
-    },
-    formData: {
-      status: content,
-      in_reply_to_id: doReply ? queryStatusId : ''
-    }
-  }
-  request(postStatusOptions, function (error, _response, body) {
-    if (error) throw new Error(error);
-    console.log(body);
-  });
+  await superagent.post('https://erica.moe/api/v1/statuses')
+    .set('Authorization', token)
+    .set('Content-Type', 'multipart/form-data')
+    .field('status', content)
+    .field('in_reply_to_id', doReply ? queryStatusId : doReplySelf ? selfStatusId : '')
+    .then((res) => {
+      selfStatusId = JSON.parse(res.text).id
+      console.log(selfStatusId)
+      console.log("-----postStatus-----\n" + res.text + "\n");
+    })
 }
 
 function getRandomInt(min, max) {
@@ -223,4 +188,4 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (realMax - realMin) + realMin);
 }
 
-main();
+setTimeout(main, 100)
